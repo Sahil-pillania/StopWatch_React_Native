e1f902cd640d49b4c7e41750eff351589fcdb6b4
Sahil pillania <sahilpillania88@gmail.com>
2023-01-21 12:15:09 +0530

    if-else for oneTime screen vs mainpage

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 // import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, StatusBar } from "react-native";
 import Screen1 from "./Views/Screen1";
+import MainScreen from "./Views/MainScreen";
+import { useState } from "react";
 
 export default function App() {
+  const [active, setActive] = useState(false);
+
+  setTimeout(() => {
+    setActive(true);
+  }, 2000);
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      <Screen1 />
+      {active ? <Screen1 /> : <MainScreen />}
     </View>
   );
 }
