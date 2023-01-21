06cceb05804442edeee7f669955eae018dc73185
Sahil pillania <sahilpillania88@gmail.com>
2023-01-21 12:01:32 +0530

    Buttons UI

--- Views/Screen1.js
+++ Views/Screen1.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, StatusBar } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
 
 const Screen1 = () => {
   const [hour, setHour] = useState("00");
@@ -54,6 +60,15 @@ const Screen1 = () => {
       <Text style={styles.min}>{min}</Text>
       <Text style={styles.sec}>{sec}</Text>
       <Text style={styles.time}>{time}</Text>
+
+      <View style={styles.bottomButtons}>
+        <TouchableOpacity>
+          <Text style={styles.button}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.button}>-</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -86,6 +101,27 @@ const styles = StyleSheet.create({
     color: "gray",
     fontSize: 120,
     fontWeight: "bold",
+  },
+  bottomButtons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    // position: "absolute",
+    // bottom: 0,
+  },
+  button: {
+    color: "white",
+    fontSize: 40,
+    margin: 20,
+    marginHorizontal: 20,
+    // padding: 10,
+    borderRadius: 90,
+    backgroundColor: "#ccc",
+    width: 60,
+    height: 60,
+    textAlign: "center",
+
+    // paddingHorizontal: 30,
   },
 });
 
